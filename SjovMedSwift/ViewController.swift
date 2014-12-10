530dc550c74dc0082840c3bf9567dc4de8e21abe
Kenneth Geisshirt <kg@realm.io>
2014-12-10 22:21:41 +0100

    Initial commit

---
+++ SjovMedSwift/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  SjovMedSwift
+//
+//  Created by Kenneth  Geisshirt on 09/12/14.
+//  Copyright (c) 2014 AltOmData. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        var n = 100
+        var sum = 0
+        for (var i = 1; i <= n; i++) {
+            sum += i
+        }
+
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
