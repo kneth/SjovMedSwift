3755793f849e44b61ea52820ce6da59f8481c144
Kenneth Geisshirt <kg@realm.io>
2015-01-05 07:15:43 +0100

    Adding Realm

---
+++ SjovMedSwift/Website.swift
@@ -0,0 +1,15 @@
+//
+//  Website.swift
+//  SjovMedSwift
+//
+//  Created by Kenneth  Geisshirt on 04/01/15.
+//  Copyright (c) 2015 AltOmData. All rights reserved.
+//
+
+import Foundation
+import Realm
+
+class Website: RLMObject {
+    dynamic var website = ""
+    dynamic var size = 0
+}
